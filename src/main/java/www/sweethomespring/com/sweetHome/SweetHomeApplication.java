0c43c8546ff59b985f4c8605006f258fc4606c65
Hang Tu Wong Ley <hangtuwlf@gmail.com>
2019-03-20 18:19:48 -0600

    first commit

---
+++ src/main/java/www/sweethomespring/com/sweetHome/SweetHomeApplication.java
@@ -0,0 +1,13 @@
+package www.sweethomespring.com.sweetHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/www/sweethomespring/com/sweetHome/SweetHomeApplicationTests.java
@@ -0,0 +1,16 @@
+package www.sweethomespring.com.sweetHome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SweetHomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
